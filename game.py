8c92d0c45adbbd14cb3823dbf37feb0d7bb2a80a
lvcalck <ludericvancalck@gmail.com>
2021-12-28 12:13:02 +0100

    added HDG_T class for game with threshold

--- game.py
+++ game.py
@@ -1,5 +1,5 @@
 class HDG:
-    def __init__(self, N: int, n_doves: int, c_h, R: float = 1.0):
+    def __init__(self, N: int, n_doves: int, c_h: float, R: float = 1.0):
         """
         Creates an N-player Hawk-Dove Game. Parameter R is assumed to be 1.
         :param N: number of players
@@ -24,9 +24,35 @@ class HDG:
             return (self.R - (self.n_hawks - 1) * self.c_h) / self.n_hawks, 0.0
 
 
+class HDG_T(HDG):
+    """
+    Creates an N-player Hawk-Dove Game with threshold above which doves can aggregate and protect the resource.
+    Parameter R is assumed to be 1.
+    :param N: number of players
+    :param n_doves: number of doves
+    :param c_h: fighting cost for hawks
+    :param c_d: protecting cost for doves
+    :param T: threshold for which dove are sufficiently many to protect resource from hawks
+    :param R: resource (=1.0 by default)
+    """
+    def __init__(self, N: int, n_doves: int, c_h: float, c_d: float, T: float, R: float = 1.0):
+        super().__init__(N, n_doves, c_h, R)
+        self.c_d = c_d
+        self.T = T
+
+    def expected_payoffs(self) -> (float, float):
+        if self.n_doves/self.N >= self.T:
+            return 0.0, (self.R - self.n_hawks * self.c_d)/self.n_doves
+        else:
+            return super().expected_payoffs()
+
+
 if __name__ == '__main__':
     # petits tests pour verifier que ça fonctionne
     print(HDG(30, 30, 0.5).expected_payoffs())
     print(HDG(30, 29, 0.5).expected_payoffs())
     print(HDG(30, 28, 0.5).expected_payoffs())
     print(HDG(30, 25, 0.5).expected_payoffs())
+    print(HDG_T(30, 28, 0.5, 0.2, 0.2).expected_payoffs())
+    print(HDG_T(30, 28, 0.5, 0.5, 0.2).expected_payoffs())
+    print(HDG_T(30, 28, 0.5, 0.9, 0.2).expected_payoffs())
