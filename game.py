ee3ee73479a2eddc5282cdf0a929fc000e7e74a8
rafftod <ramaluto@gmail.com>
2021-12-28 12:12:03 +0100

    Fix type hint

--- game.py
+++ game.py
@@ -1,3 +1,6 @@
+from typing import Tuple
+
+
 class HDG:
     def __init__(self, N: int, n_doves: int, c_h, R: float = 1.0):
         """
@@ -13,7 +16,7 @@ class HDG:
         self.c_h = c_h
         self.R = R
 
-    def expected_payoffs(self) -> (float, float):
+    def expected_payoffs(self) -> Tuple[float]:
         """
         Calculates the expected payoff for the game.
         :return: tuple (hawk_reward, dove_reward)
@@ -24,7 +27,7 @@ class HDG:
             return (self.R - (self.n_hawks - 1) * self.c_h) / self.n_hawks, 0.0
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     # petits tests pour verifier que ça fonctionne
     print(HDG(30, 30, 0.5).expected_payoffs())
     print(HDG(30, 29, 0.5).expected_payoffs())
