2a051467486e2d04e487b509598b8f1f72c5cb39
Alexandre Missenard <alexandremissenard@gmail.com>
2022-01-07 00:05:49 +0100

    graph

--- network.py
+++ network.py
@@ -225,7 +225,7 @@ if __name__ == "__main__":
         x = 0.5
         radius_list = [i for i in range(1,5)]
         R = 1.0
-        c_h_list = [i/100 for i in range(0,101,2)]
+        c_h_list = [i/100 for i in range(0,101)]
         results = []
         mode = InfiniteNPlayerHDGNetworkDynamic.CONST_MODE_SW
         
